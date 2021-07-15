c7ecd76271a1e6727518a34d0a840c01fb67dd92
EssencialSoft02 <atendimento02@essencialsoft.com.br>
2021-07-15 08:27:32 -0300

    Estrutura React Native e Typescript

---
+++ src/telas/TelaDeCarregamento.tsx
@@ -0,0 +1,19 @@
+import react from "react"
+import { View, Text, StyleSheet, Dimensions, Image} from 'react-native'
+
+export const TelaDeCarregamento = () => {
+    return (
+        <View>
+            <Text>Tela de Carregamento</Text>
+        </View>
+    )
+}
+
+const styles =  StyleSheet.create({
+    container: {
+        flex: 1
+    },
+    navigation: {
+        flex: 2
+    }
+})
